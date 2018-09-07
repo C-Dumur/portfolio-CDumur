39f28d3319545d0c043db6cc52f71f3d915af91d
C.Dumur <clement.dumur12@gmail.com>
2018-09-07 16:50:16 +0200

    Section compétences version 1

---
+++ js/generateCircle.js
@@ -0,0 +1,228 @@
+
+// Section PHP
+var php_circle = Circles.create({
+    id: 'php_circle',
+    radius: 50,
+    value: 65,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    //text: function (value) { return value + '%'; },
+    colors: ['#f9fafa', '#6699cc'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var cake_circle = Circles.create({
+    id: 'cake_circle',
+    radius: 50,
+    value: 70,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#f9fafa', '#6699cc'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var symfony_circle = Circles.create({
+    id: 'symfony_circle',
+    radius: 50,
+    value: 45,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#f9fafa', '#6699cc'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+// Section Python
+var python_circle = Circles.create({
+    id: 'python_circle',
+    radius: 50,
+    value: 60,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#34343e', '#ec5f66'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var django_circle = Circles.create({
+    id: 'django_circle',
+    radius: 50,
+    value: 55,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#34343e', '#ec5f66'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var symfony_circle = Circles.create({
+    id: 'flask_circle',
+    radius: 50,
+    value: 45,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#34343e', '#ec5f66'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+// Section JS
+var js_circle = Circles.create({
+    id: 'js_circle',
+    radius: 50,
+    value: 60,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#34343e', '#99c794'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var jquery_circle = Circles.create({
+    id: 'jquery_circle',
+    radius: 50,
+    value: 55,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#34343e', '#99c794'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var node_circle = Circles.create({
+    id: 'node_circle',
+    radius: 50,
+    value: 45,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#34343e', '#99c794'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+// Section Front
+
+var html_circle = Circles.create({
+    id: 'html_circle',
+    radius: 50,
+    value: 65,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    //text: function (value) { return value + '%'; },
+    colors: ['#f9fafa', '#cb95c6'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var css_circle = Circles.create({
+    id: 'css_circle',
+    radius: 50,
+    value: 55,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#f9fafa', '#cb95c6'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var bootstrap_circle = Circles.create({
+    id: 'bootstrap_circle',
+    radius: 50,
+    value: 65,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#f9fafa', '#cb95c6'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
+
+var prepro_circle = Circles.create({
+    id: 'prepro_circle',
+    radius: 50,
+    value: 30,
+    maxValue: 100,
+    width: 10,
+    text: '',
+    colors: ['#f9fafa', '#cb95c6'],
+    duration: 400,
+    wrpClass: 'circles-wrp',
+    textClass: 'circles-text',
+    valueStrokeClass: 'circles-valueStroke',
+    maxValueStrokeClass: 'circles-maxValueStroke',
+    styleWrapper: true,
+    styleText: true
+});
