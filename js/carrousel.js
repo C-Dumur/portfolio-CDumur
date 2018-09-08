55e3b4d245e397a46b1223cdca9ab444f544ae83
C.Dumur <clement.dumur12@gmail.com>
2018-09-08 11:56:40 +0200

    Initialisation slick.js

---
+++ js/carrousel.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#my_works').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        nextArrow: "<button type = \"button\" class= \"slick-next\" > next</button >",
+    })
+});
