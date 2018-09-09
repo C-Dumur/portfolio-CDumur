86abe2135228bec74af691a2cf6095568d28f847
C.Dumur <clement.dumur12@gmail.com>
2018-09-09 11:32:11 +0200

    creation carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+    var prevArrow = $('#prev_btn');
+    var nextArrow = $('#next_btn');
+    var toolBox = $('#btn_div')
+    console.log(prevArrow);
     $('#my_works').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: true,
-        nextArrow: "<button type = \"button\" class= \"slick-next\" > next</button >",
+        appendArrows: toolBox,
+        nextArrow: "<button class=\"btn-works\" id=\"next_arrow\">>></button>",
+        prevArrow:"<button class=\"btn-works\" id=\"prev_arrow\"><<</button></button>",
     })
 });
